891681518b7b42cc225aa2c5e8ac3b11f072bea4
CCandruss <ccandruss@gmail.com>
2020-07-14 19:59:01 -0500

    breaktime/need help

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const gallery = document.getElementById('gallery');
 const placeHolder = document.getElementById('placeholder');
 const modal = document.querySelector('.modal-container');
 const exit = document.getElementById("modal-close-btn");
+const cards = document.getElementsByClassName('card');
+console.log(cards.length);
 gallery.style.visibility = 'hidden';
 
 //fetching data
@@ -17,13 +19,14 @@ function fetch12 (url) {
     for(let i = 0; i < 12; i++){
     fetch(user)
     .then( info => info.json() )
-    .then( info => makeCard(info))
+    .then( info => makeCard(info) )
     .catch(err => console.log('There was an error retrieving users', err))
     };
 }
 
 function makeCard (data) {
     const employeeInfo = data.results;
+    
     let html = '';
     html += `  <div class="card">
     <div class="card-img-container">
@@ -36,16 +39,36 @@ function makeCard (data) {
     </div>
 </div>`
 gallery.innerHTML += html;
+return employeeInfo;
 }
 
-function showCard() {
-    const cards = document.getElementById('card');
-    console.log(cards.length);
+function makeModal(person){
+    const info = person.textContent;
+    const modalContainer = document.getElementsByClassName('modal-container');
+    
+    console.log(info);
+    // let html = '';
+    // html +=` 
+    // <div class="modal">
+    //                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    //                 <div class="modal-info-container">
+    //                     <img class="modal-img" src="${info[0].picture.medium}" alt="profile picture">
+    //                     <h3 id="name" class="modal-name cap">${info[0].name.first} ${info[0].name.last}</h3>
+    //                     <p class="modal-text">${info[0].email}</p>
+    //                     <p class="modal-text cap">${info[0].location.city}</p>
+    //                     <hr>
+    //                     <p class="modal-text">${info[0].cell}</p>
+    //                     <p class="modal-text">${info[0].location.street.number} ${info[0].location.street.name},${info[0].location.city}, ${info[0].location.state} ${info[0].location.postcode}</p>
+    //                     <p class="modal-text">Birthday: ${info[0].dob}</p>
+    //                 </div>
+    //             </div>`
+
 }
+
 
 
 // event listeners
-document.addEventListener('load', fetch12(user));
+document.addEventListener('load',fetch12(user) );
 
 setTimeout(function(){
     gallery.style.visibility = 'visible';
@@ -53,4 +76,29 @@ setTimeout(function(){
 }, 1500)
 
 exit.addEventListener('click', () => modal.style.display = 'none');
+
+
+gallery.addEventListener('click', e => {
+    const box = e.target;
+    let result;
+    if(box.className === 'card-info-container'){
+        result = box.firstElementChild;
+    } else if (box.className === 'card-img-container') {
+        result = box.parentNode.lastElementChild.firstElementChild;
+    }else if (box.className === 'card-img') {
+            result = box.parentNode.nextElementSibling.firstElementChild;
+    } else if(box.className === 'card-text'){
+        result =  box.previousElementSibling;
+    }else if(box.className === 'card'){
+            result =  box.firstElementChild;    
+    } else if(box.className === 'card-text cap'){
+        result =  box.previousElementSibling.previousElementSibling;
+    } else if(box.className === 'gallery'){
+        result =  box.firstElementChild.lastElementChild.firstElementChild;
+    } else {
+        result = box;
+    }
+    makeModal(result);
+});
+
 //post?
