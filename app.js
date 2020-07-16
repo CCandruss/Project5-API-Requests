bef94cd076cda34aafb84bb7d46bd7de85f841ec
CCandruss <ccandruss@gmail.com>
2020-07-16 15:46:06 -0500

    final commit

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ const user = 'https://randomuser.me/api/';
 const gallery = document.getElementById('gallery');
 const placeHolder = document.getElementById('placeholder');
 const modal = document.querySelector('.modal-container');
-const exit = document.getElementById("modal-close-btn");
-const cards = document.getElementsByClassName('card');
-console.log(cards.length);
 gallery.style.visibility = 'hidden';
 
-//fetching data
-
-// fetch(user)
-//     .then( info => info.json() )
-//     .then( info => makeCard(info))
-
-
 // functions
+//fetching data
 function fetch12 (url) {
     for(let i = 0; i < 12; i++){
     fetch(user)
@@ -24,60 +15,83 @@ function fetch12 (url) {
     };
 }
 
+//creates employee info cards as well as getting my modal info to use later since I am not posting info to server and need it for modal window
 function makeCard (data) {
     const employeeInfo = data.results;
     
     let html = '';
-    html += `  <div class="card">
+    html += 
+    `<div id='${employeeInfo[0].name.first} ${employeeInfo[0].name.last}' class="card">
+        
     <div class="card-img-container">
         <img class="card-img" src='${employeeInfo[0].picture.medium}' alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${employeeInfo[0].name.first} ${employeeInfo[0].name.last}</h3>
-        <p class="card-text">${employeeInfo[0].email}</p>
-        <p class="card-text cap">${employeeInfo[0].location.city}, ${employeeInfo[0].location.state}</p>
-    </div>
+        </div>
+
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${employeeInfo[0].name.first} ${employeeInfo[0].name.last}</h3>
+            <p class="card-text">${employeeInfo[0].email}</p>
+            <p class="card-text cap">${employeeInfo[0].location.city}, ${employeeInfo[0].location.state}</p>
+        </div>
+
+        <div class="extra-info">
+            <p id="phone" class="hide">${employeeInfo[0].cell}</p>
+            <p id='shipping' class="hide">${employeeInfo[0].location.street.number} ${employeeInfo[0].location.street.name}, ${employeeInfo[0].location.city}, ${employeeInfo[0].location.state} ${employeeInfo[0].location.postcode} </p>
+            <p id='bday' class="card-text cap">${employeeInfo[0].dob.date}</p>
+        </div>
 </div>`
 gallery.innerHTML += html;
-return employeeInfo;
 }
-
+//creates the modal window as well as formatting birthdays
 function makeModal(person){
-    const info = person.textContent;
-    const modalContainer = document.getElementsByClassName('modal-container');
-    
-    console.log(info);
-    // let html = '';
-    // html +=` 
-    // <div class="modal">
-    //                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    //                 <div class="modal-info-container">
-    //                     <img class="modal-img" src="${info[0].picture.medium}" alt="profile picture">
-    //                     <h3 id="name" class="modal-name cap">${info[0].name.first} ${info[0].name.last}</h3>
-    //                     <p class="modal-text">${info[0].email}</p>
-    //                     <p class="modal-text cap">${info[0].location.city}</p>
-    //                     <hr>
-    //                     <p class="modal-text">${info[0].cell}</p>
-    //                     <p class="modal-text">${info[0].location.street.number} ${info[0].location.street.name},${info[0].location.city}, ${info[0].location.state} ${info[0].location.postcode}</p>
-    //                     <p class="modal-text">Birthday: ${info[0].dob}</p>
-    //                 </div>
-    //             </div>`
-
+    const extras = person.parentNode.parentNode;
+    const cards = document.getElementsByClassName('card');
+    for(let i = 0; i < cards.length; i++){
+        if(cards[i].id === person.textContent){
+            let dob = cards[i].lastElementChild.lastElementChild.textContent;
+            var cleaned = ('' + dob.replace(/\D/g, '') )
+            console.log(cleaned);
+            var match = cleaned.replace(/^(\d{4})(\d{2})(\d{2})(\d{9})$/, '$2/$3/$1' )
+            console.log(match)
+            dob = match
+            let html = '';
+            html +=` 
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${cards[i].firstElementChild.firstElementChild.src}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${cards[i].firstElementChild.nextElementSibling.firstElementChild.textContent}</h3>
+                    <p class="modal-text">${cards[i].firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.textContent}</p>
+                    <p class="modal-text cap">${cards[i].firstElementChild.nextElementSibling.lastElementChild.textContent}</p>
+                    <hr>
+                    <p class="modal-text">${cards[i].lastElementChild.firstElementChild.textContent}</p>
+                    <p class="modal-text">${cards[i].lastElementChild.firstElementChild.nextElementSibling.textContent}</p>
+                    <p id ='dob' class="modal-text">Birthday: ${dob}</p>
+                </div>
+            </div>`
+            modal.innerHTML = html;
+            modal.style.display = 'inherit'
+        }   
+    }
 }
-
-
 
 // event listeners
+//creates user elements runs on page load
 document.addEventListener('load',fetch12(user) );
 
+//added so div elements displayed after all are loaded
 setTimeout(function(){
     gallery.style.visibility = 'visible';
     placeHolder.style.display = 'none';
 }, 1500)
 
-exit.addEventListener('click', () => modal.style.display = 'none');
+//close modal window
+modal.addEventListener('click', (e) => {
+    if(e.target.textContent === 'X'){
+        modal.style.display = 'none';
+    }
+});
 
-
+//creating an event listener that can return the name of the user to feed to makeModal
 gallery.addEventListener('click', e => {
     const box = e.target;
     let result;
@@ -95,10 +109,10 @@ gallery.addEventListener('click', e => {
         result =  box.previousElementSibling.previousElementSibling;
     } else if(box.className === 'gallery'){
         result =  box.firstElementChild.lastElementChild.firstElementChild;
+    } else if(box.className === 'extra-info'){
+        result =  box.previousElementSibling.firstElementChild;
     } else {
         result = box;
     }
     makeModal(result);
 });
-
-//post?
